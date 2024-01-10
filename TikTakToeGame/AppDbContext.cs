bd8c2ab40688c04b7ad2f61b2a09550843f8ddd8
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-01-10 13:19:52 +0200

    Add AppDbContext

---
+++ TikTakToeGame/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+using TikTakToeGame.Entities;
+
+namespace TikTakToeGame
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Field> Fields { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Turn> Turns { get; set; }
+     
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+   
+}
