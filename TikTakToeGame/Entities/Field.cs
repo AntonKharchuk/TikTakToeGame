8990c3619365c9b9eb10fdc1e431ebe9547541ed
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-01-10 02:06:38 +0200

    Add project files.

---
+++ TikTakToeGame/Entities/Field.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TikTakToeGame.Entities
+{
+    public class Field
+    {
+        public int Id { get; set; }
+        public int[,] Positions { get; set; }
+        public int StatusId { get; set; }
+        public int TurnId { get; set; }
+    }
+    public class Field
+    { 
+    }
+}
