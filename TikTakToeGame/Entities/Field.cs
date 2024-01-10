af3ac5012378039fa5923eacfad77a9a6d661bd2
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-01-10 13:43:03 +0200

    Add coment to define Positions

--- TikTakToeGame/Entities/Field.cs
+++ TikTakToeGame/Entities/Field.cs
@@ -3,7 +3,7 @@ namespace TikTakToeGame.Entities
 {
     public class Field: BaseEntity
     {
-        public int[,] Positions { get; set; }
+        public int[,] Positions { get; set; }//0 none, 1 X, 2 O
         public int StatusId { get; set; }
         public int TurnId { get; set; }
     }
