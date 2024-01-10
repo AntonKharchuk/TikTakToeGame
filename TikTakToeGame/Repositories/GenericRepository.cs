6c4c39a2b2e410f063947ed3514f8f446a8e5819
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-01-10 13:20:15 +0200

    Add GenericRepositories

---
+++ TikTakToeGame/Repositories/GenericRepository.cs
@@ -0,0 +1,87 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using TikTakToeGame.Entities;
+using TikTakToeGame.Exceptions;
+
+namespace TikTakToeGame.Repositories
+{
+    
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _context;
+
+        private DbSet<T> _table;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _table = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(string includes = "")
+        {
+            var query = _table.AsQueryable();
+            query = IncludeFields(includes, query);
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id, string includes = "")
+        {
+            var query = _table.AsQueryable();
+
+            query = IncludeFields(includes, query);
+
+            return await query.FirstOrDefaultAsync(g => g.Id == id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            ThrowIfNull(entity);
+            await _table.AddAsync(entity);
+        }
+
+        public async Task UpdateAsync(T entity, int id)
+        {
+            ThrowIfNull(entity);
+            if (entity.Id != id)
+            {
+                throw new ArgumentException(nameof(entity), "entity.Id and id missmatch");
+            }
+            var tableEntity = await GetByIdAsync(id);
+            ThrowIfNull(tableEntity);
+            _context.Entry(tableEntity).CurrentValues.SetValues(entity);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var tableEntity = await GetByIdAsync(id);
+            ThrowIfNull(tableEntity);
+            _table.Remove(tableEntity);
+        }
+        private static void ThrowIfNull(T? entity)
+        {
+            if (entity is null)
+            {
+                throw new EntityNotFoundException("Entity with the specified ID not found.");
+            }
+        }
+        private static IQueryable<T> IncludeFields(string includes, IQueryable<T> query)
+        {
+            if (!string.IsNullOrEmpty(includes))
+            {
+                foreach (var include in includes.Split(","))
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return query;
+        }
+    }
+}
---
+++ TikTakToeGame/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using TikTakToeGame.Entities;
+
+namespace TikTakToeGame.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync(string include = "");
+        Task<T>? GetByIdAsync(int id, string include = "");
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity, int id);
+        Task DeleteAsync(int id);
+        Task SaveAsync();
+    }
+}
