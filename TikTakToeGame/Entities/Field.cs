600886c4bbf9ecadde7e40ee366e2e2657543304
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-01-12 16:32:02 +0200

    Change Entity field to one that is supported with EF

--- TikTakToeGame/Entities/Field.cs
+++ TikTakToeGame/Entities/Field.cs
@@ -3,9 +3,19 @@ namespace TikTakToeGame.Entities
 {
     public class Field: BaseEntity
     {
-        public int[,] Positions { get; set; }//0 none, 1 X, 2 O
+        public List<int>? Positions { get; set; }//0 none, 1 X, 2 O
+        /*
+        0,1,2
+        3,4,5
+        6,7,8
+         */
+
         public int StatusId { get; set; }
+        public Status? Status { get; set; }
+
         public int TurnId { get; set; }
+        public Turn? Turn { get; set; }
+
     }
 
 }
