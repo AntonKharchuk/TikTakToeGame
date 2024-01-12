6bb65ffe8d2bb2f58ddecf8fc891d6da16d124d8
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-01-12 18:44:04 +0200

    set business logic for Field

--- TikTakToeGame.Business/Services/FieldService.cs
+++ TikTakToeGame.Business/Services/FieldService.cs
@@ -15,6 +15,7 @@ namespace TikTakToeGame.Business.Services
         {
             var newField = new Field()
             {
+                Positions = "0,0,0,0,0,0,0,0,0",
                 StatusId = 1,
                 TurnId = 1,
             };
@@ -34,15 +35,84 @@ namespace TikTakToeGame.Business.Services
 
         public async Task UpdateItemAsync(string positions, int id)
         {
-            if (positions.Length!=9)
-            {
-                throw new ArgumentException();
-            }
             var tableItem =  await _genericRepository.GetByIdAsync(id);
 
             tableItem.Positions = positions;
 
+            UpdateTurnAndStatus(tableItem);
+
             await _genericRepository.SaveAsync();
         }
+        private void UpdateTurnAndStatus(Field field)
+        {
+            field.TurnId = field.TurnId == 1 ? 2 : 1;
+
+            var positions = new int[9];
+            
+            var strMarks = field.Positions.Split(',');
+            if (strMarks.Length != 9)
+            {
+                throw new ArgumentException();
+            }
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = int.Parse(strMarks[i]);
+            }
+
+            if (CheckForWin(positions, 1))
+            {
+                field.StatusId = 2;
+                return;
+            }
+            if (CheckForWin(positions, 2))
+            {
+                field.StatusId = 3;
+                return;
+            }
+
+            if (CheckForTie(positions))
+            {
+                field.StatusId = 4;
+                return;
+            }
+        }
+        private bool CheckForTie(int[] positions)
+        {
+            foreach (var mark in positions)
+            {
+                if (mark==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool CheckForWin(int[] positions, int mark)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (positions[i] == mark && positions[i+3] == mark && positions[i+6] == mark)
+                {
+                    return true;
+                }
+            }
+            for (int i = 0; i < 8; i+=3)
+            {
+                if (positions[i] == mark && positions[i + 1] == mark && positions[i + 2] == mark)
+                {
+                    return true;
+                }
+            }
+            if (positions[0] == mark && positions[4] == mark && positions[8] == mark)
+            {
+                return true;
+            }
+            if (positions[2] == mark && positions[4] == mark && positions[6] == mark)
+            {
+                return true;
+            }
+            return false;
+        }
+
     }
 }
